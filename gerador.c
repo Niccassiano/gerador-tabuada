c4abb18432cfa7937883902ccd1a2fc6fde09920
Niccassiano <nicolle.cassianoti@gmail.com>
2026-06-18 21:33:27 -0300

    feat: Criar função da tabuada

--- gerador.c
+++ gerador.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
+void gerarTabuada(int numero) {
+    for(int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", numero, i, numero * i);
+    }
+}
+
 int main (){
     
     // primeiro a tabuada:
     
-    int numero, i;
+    int numero;
     
     printf("Digite a tabuada que deseja:\n");
     scanf("%d", &numero);
+
+    gerarTabuada(numero);
     
-    for(i = 1; i <= 10; i++){
-        printf("%d * %d = %d\n", numero, i, i * numero);
-    }
+
+    // for(i = 1; i <= 10; i++){
+        // printf("%d * %d = %d\n", numero, i, i * numero);
+   //  }
     
     return 0;
 }
