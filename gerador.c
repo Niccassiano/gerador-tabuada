db0901b58e1018ebfc737f0926ef4772a7b2aa9b
Nicolle Cassiano <niccassiano@MacBook-Air-de-Nicolle.local>
2026-07-01 20:31:04 -0300

    feat: implementa gerador de tabuada com exportacao em arquivo

--- gerador.c
+++ gerador.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 
-void gerarTabuada(int numero) {
+void gerarTabuada(int numero){
     for(int i = 1; i <= 10; i++){
         printf("%d x %d = %d\n", numero, i, numero * i);
     }
 }
 
-int main (){
-    
-    // primeiro a tabuada:
-    
+int main(){
     int numero;
-    
-    printf("Digite a tabuada que deseja:\n");
+
+    printf("Digite um numero: ");
     scanf("%d", &numero);
 
     gerarTabuada(numero);
-    
 
-    // for(i = 1; i <= 10; i++){
-        // printf("%d * %d = %d\n", numero, i, i * numero);
-   //  }
-    
+    FILE *arquivo;
+    arquivo = fopen("tabuada.txt", "w");
+
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    for(int i = 1; i <= 10; i++){
+        fprintf(arquivo, "%d x %d = %d\n", numero, i, numero * i);
+    }
+
+    fclose(arquivo);
+
+    printf("\nArquivo criado com sucesso!\n");
+
     return 0;
 }
